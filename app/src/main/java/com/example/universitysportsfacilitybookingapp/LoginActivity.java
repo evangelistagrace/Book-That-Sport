90eb2159052ef887692fe5bf33b0908d5fc13a44
Grace <evangrace98@gmail.com>
2021-09-15 15:56:45 +0800

    test commit

--- app/src/main/java/com/example/universitysportsfacilitybookingapp/LoginActivity.java
+++ app/src/main/java/com/example/universitysportsfacilitybookingapp/LoginActivity.java
@@ -3,5 +3,5 @@ package com.example.universitysportsfacilitybookingapp;
 import android.widget.EditText;
 
 public class LoginActivity {
-    EditText et_cust_username, et_cust_passwordd;
+    EditText et_cust_username, et_cust_password;
 }
